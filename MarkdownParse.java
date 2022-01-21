cc075d7f93e05b8cfb3e6571911362edf8ab2d59
vismaymanoj <vmanoj@ucsd.edu>
2022-01-20 18:58:41 -0800

    new edits

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,15 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if( nextOpenBracket==-1)
+            break;
+            System.out.println("Beggining loop Current index: " + currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1; //one after closed paren
+            System.out.println("Current index: " + currentIndex);
         }
         return toReturn;
     }
